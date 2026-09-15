33291848e39e769ff0a775685b5d5d5776bc1f01
Mitchell Wilson <mitchellwilson@Mitchells-MacBook-Pro.local>
2026-09-15 13:37:15 -0500

    feat: add more rules on marking letters are in-word or correct position

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Board from './Board'
 import Line from './Line'
 import useWordleGame from './hooks/useWordleGame'
 import { EMPTY_GUESS, MAX_LETTER_LENGTH, MAX_FUTURE_ROWS_LENGTH } from './constants'
+import { count } from './utils'
 
 function App() {
   const { currentGuess, gameover, guesses, solution } = useWordleGame()
@@ -15,28 +16,59 @@ function App() {
         gameover ? <h2>GAME OVER <button onClick={() => window.location.reload()}>RESTART</button></h2> : ''
       }
       <Board>
-        { // This section is for past guesses --------
+        { // This section is for past guesses
           newGuesses.map(( guess, i ) => {
             const className = "tile"
             const tiles = []
+            const countCorrectLettersInGuess = {};
+            const countCorrectLettersInSolution = count(solution);
+            
+            // First pass - add correct styles for letters in word in correct position
             for(let i=0; i<guess.length; i++) {
               let stateName = ''
               let character = guess[i]
               if( character === solution[i]) {
                 stateName = 'correct'
+                countCorrectLettersInGuess[character] = countCorrectLettersInGuess[character] ? countCorrectLettersInGuess[character] + 1 : 1
               } else if ( character === ' ' ) {
                 stateName = 'unknown'
               } else if ( solution.indexOf(guess[i]) > -1 ) {
-                stateName = 'in-word'
+                // stateName = 'in-word'
               } else {
                 stateName = 'incorrect'
               }
               tiles.push(<div key={i} className={ `${className} ${stateName} past` }>{guess[i].toUpperCase()}</div>)
             }
-            return <Line key={i}>{ tiles }</Line>
+            // Second pass - add any relevant "in-word" styles for letters in word not in correct position
+
+            
+
+            const updatedTiles = tiles.map((node, i) => {
+
+              // How many times is this character in the solution?
+              // And how many times is it in the guess in the correct position?
+
+              const char = node.props.children.toLowerCase()
+              const isThisCharInThisPositionInSolution = char === solution[i]
+              const isThisCharInSolution = solution.indexOf(char) > -1 
+              if ( ! isThisCharInThisPositionInSolution && 
+                  isThisCharInSolution && 
+                ( countCorrectLettersInGuess[char] < countCorrectLettersInSolution[char] ) 
+              ) {
+                return <div
+                  key={ node.key }
+                  className={ `${node.props.className} in-word` }
+                  >
+                    { node.props.children }
+                </div>
+              } else {
+                return node
+              }
+            })
+            return <Line key={i}>{ updatedTiles  }</Line>
           })
         }
-        { // This section is for the current guess ---------------------
+        { // This section is for the current guess
           <Line key="current-guess">
             {
               (()=>{
@@ -53,7 +85,7 @@ function App() {
             }
           </Line>
         }
-        { // This section is for the future guesses -------
+        { // This section is for the future guesses
           (()=>{
             let i = gameover ? -1 : 0 // i needs to be set based on final result for UI number of rows
             const lines = []
